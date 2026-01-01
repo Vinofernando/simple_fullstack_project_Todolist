11e0a1e7458c53771ed5161c243175f0c917a743
Vino <vinofernando84@gmail.com>
2026-01-01 19:56:10 +0700

    switch config to supabase

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
 
 export default pool
--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,14 @@ import app from './app.js'
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+import pool from "./config/db.js"; // sesuaikan path
+
+(async () => {
+  try {
+    await pool.query("SELECT 1");
+    console.log("✅ Database Supabase CONNECTED");
+  } catch (error) {
+    console.error("❌ Database connection FAILED");
+    console.error(error.message);
+  }
+})();
